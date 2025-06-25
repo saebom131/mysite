e5a97ed8245836b12787a5a023f676e138cb9d08
toqha <ojds01@gmail.com>
2025-06-25 14:24:20 +0900

    update

---
+++ db_setting.py
@@ -0,0 +1,20 @@
+import pymysql
+from database import MyDB
+
+# MyDB class 생성
+mydb = MyDB()
+
+# table 생성 쿼리문
+create_user = '''
+    create table
+    if not exists
+    user (
+    id varchar(32) primary key,
+    password varchar(64) not null,
+    name varchar(32)
+    )
+'''
+# sql 쿼리문 실행
+mydb.sql_query(create_user)
+# db 서버에 동기화하고 연결 종료
+mydb.commit_db()
